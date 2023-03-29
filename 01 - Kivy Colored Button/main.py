322ccf8900c7309ef686d7df429bf37042e50723
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-03-29 13:37:42 -0500

    Add files via upload

---
+++ 01 - Kivy Colored Button/main.py
@@ -0,0 +1,21 @@
+# Using Python 10
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+# Notes:
+# https://kivy.org/doc/stable/api-kivy.uix.button.html
+# x,y coordinate system starts at bottom left
+# button size defaults to 100 by 100
+
+
+# Interface
+class Interface(Widget):
+    pass
+
+
+# App Creation
+class TestApp(App):
+    pass
+
+
+TestApp().run()
