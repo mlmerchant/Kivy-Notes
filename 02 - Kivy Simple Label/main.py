79024054ee824c9f79b724682892933b5f16ae02
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-03-29 16:45:53 -0500

    Add files via upload

---
+++ 02 - Kivy Simple Label/main.py
@@ -0,0 +1,20 @@
+# Using Python 10
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+# Notes:
+# x,y coordinate system starts at bottom left
+# button size defaults to 100 by 100
+
+
+# Interface
+class Interface(Widget):
+    pass
+
+
+# App Creation
+class TestApp(App):
+    pass
+
+
+TestApp().run()
