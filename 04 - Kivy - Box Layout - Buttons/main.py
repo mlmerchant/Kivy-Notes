63b806fa9288e11f837f0d59dea0382dd82b8c71
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-04-01 19:28:06 -0500

    Add files via upload

---
+++ 04 - Kivy - Box Layout - Buttons/main.py
@@ -0,0 +1,16 @@
+# Using Python 3.10
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+# Interface
+class Interface(BoxLayout):
+    pass
+
+
+# App Creation
+class TestApp(App):
+    pass
+
+
+TestApp().run()
